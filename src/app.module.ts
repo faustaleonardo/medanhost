b8b8a6877f262c7e47e1a70a626f67638b82b266
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-04 21:52:54 +0700

    remove unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
@@ -8,7 +7,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
