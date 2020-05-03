a70796710fe6a4b36b72edc93e2039ff6152139c
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-05-03 13:14:47 +0700

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ docker build -t faustaleonardo/medanhost:$SHA -t faustaleonardo/medanhost:latest
 # docker run -d -p 5050:5050 faustaleonardo/medanhost:$SHA
 
 docker push faustaleonardo/medanhost:latest
+docker push faustaleonardo/medanhost:$SHA
